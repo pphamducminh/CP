05adee7e1e0dfb258509614d2797d964167b40d5
Phạm Đức Minh <115942885+pphamducminh@users.noreply.github.com>
2024-03-26 21:24:47 +0700

    template

---
+++ template.cpp
@@ -0,0 +1,74 @@
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cctype>
+#include <cstring>
+#include <iomanip>
+#include <deque>
+
+using namespace std;
+
+#define  file(name)  freopen(name".inp", "r", stdin);\
+                     freopen(name".out", "w", stdout);
+#define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define TIME        (1.0 * clock() / CLOCKS_PER_SEC)
+#define all(a)      a.begin(),a.end()
+#define endl        "\n"
+#define all1(a)     a+1,a+n+1
+// #define push_back   emplace_back
+// #define gcd(a,b)    __gcd(a,b);
+// #define lcm(a,b)    (a*b)/gcd(a,b);
+
+const long long INF = (long long)1e18;
+const long long MOD = (long long)1e9+7;
+const long long MODD = 1e9; /// 998244353
+const int maxN = 1e5+55;
+const int LOG = 30;
+const int mlift = 20;
+
+
+///--------------------------------
+
+
+void solve();
+
+signed main(){
+
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        file("ducminh");
+    #endif
+    
+
+    // file("daycon");
+    
+    int t;
+
+    // cin >> t;
+
+    t=1;
+
+    while (t--){
+        solve();
+    }
+
+    cerr << "Time elapsed: " << TIME << "s.\n";
+    cerr << "ducminh" << "\n";
+    return 0;
+}
+
+
+
+///--------------------[PROBLEM SOLUTION]--------------------///
+
+
+
