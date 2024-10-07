fa9eef8c6f624235f3c578d415f3bade5658fd9e
Pham Duc Minh <115942885+pphamducminh@users.noreply.github.com>
2024-10-08 00:46:26 +0700

    Delete A_thuattoan directory

--- A_thuattoan/NenSo.cpp
+++
@@ -1,5 +0,0 @@
-    sort(all(b));
-    b.resize(unique(all(b)) - b.begin());
- 
-    for(int i = 1; i <= n; i++)
-        a[i] = lower_bound(all(b), a[i]) - b.begin() + 1;
--- A_thuattoan/print_128.cpp
+++
@@ -1,9 +0,0 @@
-void print(__int128 x){
-
-    if (x<0){
-        putchar('-');
-        x=-x;
-    }
-    if (x>9) print(x/10);
-    putchar(x%10+'0');
-}
